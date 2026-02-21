4062012c99f27eab1100ec59750159a4a8cb8d77
Rohit <rohitnarnolia88@gmail.com>
2026-02-21 12:37:11 +0530

    add: function to generate better-auth open api routes

---
+++ apps/server/src/utils/auth.ts
@@ -0,0 +1,50 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "@repo/db";
+import { openAPI, type Path } from "better-auth/plugins";
+
+export const auth = betterAuth({
+  plugins: [openAPI()],
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  secret: process.env.BETTER_AUTH_SECRET!,
+  baseURL: process.env.BETTER_AUTH_URL!,
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+    },
+  },
+  trustedOrigins: [process.env.WEB_URL!],
+});
+
+export type Auth = typeof auth;
+export const OpenAPI = auth.api;
+
+let _schema: ReturnType<typeof auth.api.generateOpenAPISchema>;
+const getSchema = async () => (_schema ??= auth.api.generateOpenAPISchema());
+
+export const BetterAuthOpenAPI = {
+  getPaths: (prefix = "/api/auth") =>
+    getSchema().then(({ paths }) => {
+      const result: typeof paths = Object.create(null);
+      for (const path of Object.keys(paths)) {
+        const key = prefix + path;
+        result[key] = paths[path] as any;
+        for (const method of Object.keys(paths[path]!)) {
+          (result[key] as any)[method].tags = ["Auth"];
+        }
+      }
+      return result;
+    }) as Promise<any>,
+  getComponents: () =>
+    getSchema().then(({ components }) => components) as Promise<any>,
+};
